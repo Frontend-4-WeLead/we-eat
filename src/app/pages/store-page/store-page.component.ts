e709ad32a172825948f22d1e2b96f24a39818640
NikosGour <nikosgournak@gmail.com>
2023-12-04 21:51:25 +0200

    Added pages folder

---
+++ src/app/pages/store-page/store-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-store-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './store-page.component.html',
+  styleUrl: './store-page.component.css'
+})
+export class StorePageComponent {
+
+}
