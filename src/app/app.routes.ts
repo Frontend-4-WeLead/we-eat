5814c251013dae08d13490babf6de5adc5a25323
Sevi Gouland <sevigouland@gmail.com>
2023-12-06 22:16:22 +0200

    add image, refactor login component, add navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginSignupPageComponent } from './pages/login-signup-page/login-signup-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginSignupPageComponent },
+    { path: '', redirectTo: 'login', pathMatch: "full" }
+];
--- src/app/pages/login-signup-page/login-signup-page.component.ts
+++ src/app/pages/login-signup-page/login-signup-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-signup-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './login-signup-page.component.html',
   styleUrl: './login-signup-page.component.css'
 })
