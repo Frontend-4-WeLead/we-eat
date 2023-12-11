00562e270a0bac94e3c57391944c813e0417b1f4
NikosGour <nikosgournak@gmail.com>
2023-12-11 21:49:00 +0200

    Added advanced routing for `store-page` page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [provideRouter(routes, withComponentInputBinding()),
   importProvidersFrom(HttpClientModule)]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginSignupPageComponent } from './pages/login-signup-page/login-signup-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { StorePageComponent } from './pages/store-page/store-page.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginSignupPageComponent },
     { path: 'stores', component: HomePageComponent },
+    { path: 'stores/:storeId', component: StorePageComponent },
     { path: '', redirectTo: 'login', pathMatch: "full" }
 ];
 
