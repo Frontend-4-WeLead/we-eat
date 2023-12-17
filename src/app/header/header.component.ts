7ff6e2f26e0729f69ecb6b4abc9d902360ac00a2
adosiou <your@email>
2023-12-17 12:31:30 +0200

    created logo and conditional header rendering

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressComponent } from "./address/address.component";
 import { UserComponent } from "./user/user.component";
+import { Router } from '@angular/router';
+
 
 @Component({
     selector: 'app-header',
@@ -11,5 +13,12 @@ import { UserComponent } from "./user/user.component";
     imports: [CommonModule, AddressComponent, UserComponent]
 })
 export class HeaderComponent {
+
+    constructor(private router: Router) {}
+
+    isLoginOrSignUpPage(): boolean {
+        const path = this.router.url;
+        return path.includes("login") || path.includes("register");
+    }
 
 }
