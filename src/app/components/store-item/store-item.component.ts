2fe4f732fd66073c8fac8b102207552086c4af3f
SeviGouland <sevigouland@gmail.com>
2023-12-24 17:53:25 +0200

    create sample data folder and add json files

--- src/app/components/store-item/store-item.component.ts
+++ src/app/components/store-item/store-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-store-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './store-item.component.html',
   styleUrl: './store-item.component.css'
 })
