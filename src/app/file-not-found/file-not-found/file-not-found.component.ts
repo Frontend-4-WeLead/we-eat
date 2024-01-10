694010d1649cbb49edb20084d4af0d150d35c684
NikosGour <nikosgournak@gmail.com>
2024-01-10 18:55:07 +0200

    FileNotFound on Storepage wrong store id

--- src/app/file-not-found/file-not-found/file-not-found.component.ts
+++ src/app/file-not-found/file-not-found/file-not-found.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-file-not-found',
   templateUrl: './file-not-found.component.html',
-  styleUrls: ['./file-not-found.component.css']
+  styleUrls: ['./file-not-found.component.css'],
+  standalone: true
 })
 export class FileNotFoundComponent implements OnInit {
 
--- src/app/pages/store-page/store-page.component.ts
+++ src/app/pages/store-page/store-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, NgModule, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './components/product/product.component';
 import { HeaderComponent } from '../../header/header.component';
@@ -6,19 +6,25 @@ import { DataService } from '../../services/data.service';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
 import { Product } from '../../models/product';
 import { FooterComponent } from '../../footer/footer.component';
+import { Router } from '@angular/router';
+import { FileNotFoundComponent } from '../../file-not-found/file-not-found/file-not-found.component';
+
 
 @Component({
   selector: 'app-store-page',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent, FooterComponent, CartItemComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent,
+    FooterComponent, CartItemComponent, FileNotFoundComponent],
   templateUrl: './store-page.component.html',
   styleUrl: './store-page.component.css'
 })
+
 export class StorePageComponent implements OnInit {
   storeInfo: any;
   data_service = inject(DataService)
   cart: Array<Product> = [];
   @Input() storeId: number = 0;
+
 
   ngOnInit(): void {
     // Took me 2 hours to find that even though in Typescript i have defined
@@ -27,7 +33,6 @@ export class StorePageComponent implements OnInit {
     this.storeId = +this.storeId;
     this.data_service.getStoreById(this.storeId).subscribe({
       next: (data) => {
-        console.log(data);
         this.storeInfo = data;
       }
     })
