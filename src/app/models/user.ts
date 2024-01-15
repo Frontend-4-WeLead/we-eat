0ba64e6d9e134775738e31e36cc5d770e415462f
Sevi Goulandri <131068519+SeviGouland@users.noreply.github.com>
2024-01-15 19:59:10 +0200

    add user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,28 @@
+export interface User {
+    id: number;
+    fisrtName: string;
+    lastName: string;
+    image_url: string;
+    order_list: Order[];
+    address_list: Address[];
+}
+
+export interface Order {
+    id: number;
+    store: string;
+    order_date: string;
+    product_list: Product[];
+}
+
+export interface Address {
+    street: string;
+    number: string;
+    district: string;
+    city: string;
+}
+
+export interface Product {
+    quantity: string;
+    product: string;
+    total_price: number;
+}
