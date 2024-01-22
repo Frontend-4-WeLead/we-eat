bf172dc33bb994d4858fa967c65b714b6325e9bb
adosiou <your@email>
2024-01-22 20:37:53 +0200

    header component: publish address through address.service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component, NgModule, inject } from '@angular/core';
+import { Component, NgModule, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressComponent } from "./address/address.component";
 import { UserComponent } from "./user/user.component";
 import { Router } from '@angular/router';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { ModalModule, ModalService } from '@developer-partners/ngx-modal-dialog';
+import { AddressService } from '../../services/address.service';
 
 @Component({
     selector: 'app-header',
@@ -13,22 +14,30 @@ import { ModalModule, ModalService } from '@developer-partners/ngx-modal-dialog'
     styleUrl: './header.component.css',
     imports: [CommonModule ,AddressComponent, UserComponent, UserDetailsComponent, ModalModule]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-    constructor(private router: Router, private readonly _modalService: ModalService) { }
+  constructor(private router: Router, private readonly _modalService: ModalService, private addressService: AddressService) { }
 
-    public showModal(): void {
-        this._modalService.show(UserDetailsComponent, {
+  ngOnInit() {
+      this.selectAddress('Ipirou 1');
+  }
+
+  public showModal(): void {
+      this._modalService.show(UserDetailsComponent, {
           title: 'Ο λογαριασμός μου'
-        });
-      }
+      });
+  }
 
-    isLoginOrSignUpPage(): boolean {
-        const path = this.router.url;
-        return path.includes("login") || path.includes("register");
-    }
+  isLoginOrSignUpPage(): boolean {
+      const path = this.router.url;
+      return path.includes("login") || path.includes("register");
+  }
 
-    searchFor(searchValue: string): void {
-    }
+  searchFor(searchValue: string): void {
+     
+  }
 
+  selectAddress(address: string) {
+      this.addressService.publish(address);
+  }
 }
