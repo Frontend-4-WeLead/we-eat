f9a2a01682ee3f1fe3e96438a4a2f400de7d2d4d
NikosGour <nikosgournak@gmail.com>
2024-01-22 19:30:58 +0200

    Made checkout better

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { ProductComponent } from '../store-page/components/product/product.component';
 import { AddressService } from '../../services/address.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-page',
@@ -15,43 +16,55 @@ export class CheckoutPageComponent implements OnInit {
   cart_service = inject(CartService)
   address_service = inject(AddressService)
   cart: Array<any> = [];
-  address: string = "";
+  address: string = "MISSING ADDRESS";
   total: number = 0;
+  router = inject(Router);
 
   ngOnInit(): void {
 
     this.cart_service.getObservable().subscribe({
       next: (data) => {
-        this.cart = data;
+        this.cart = data || this.cart;
       }
     })
 
     // PLACEHOLDER DATA
-    this.cart = [
-      {
-        "title": "Big Mac",
-        "price": 4.99
-      },
-      {
-        "title": "Crispy McBacon",
-        "price": 3.99
-      },
-      {
-        "title": "Deluxe Burger",
-        "price": 2.99
-      },
-      {
-        "title": "Deluxe Burger",
-        "price": 2.99
-      }
-    ]
-    this.total = this.cart.reduce((acc, curr) => acc + curr.price, 0);
+    // this.cart = [
+    //   {
+    //     "title": "Big Mac",
+    //     "price": 4.99
+    //   },
+    //   {
+    //     "title": "Crispy McBacon",
+    //     "price": 3.99
+    //   },
+    //   {
+    //     "title": "Deluxe Burger",
+    //     "price": 2.99
+    //   },
+    //   {
+    //     "title": "Deluxe Burger",
+    //     "price": 2.99
+    //   }
+    // ]
     // END PLACEHOLDER DATA
+    this.total = this.cart.reduce((acc, curr) => acc + curr.price, 0);
     this.address_service.getObservable().subscribe({
       next: (data) => {
-        this.address = data;
+        this.address = data || this.address;
       }
     })
   }
 
+  completePurchase() {
+    const max = 1000000;
+    const min = 100000;
+    alert(`Purchase with order number: "${(Math.random() * (max - min + 1) + min).toFixed(0)}" completed!\n` +
+      `Your order will be delivered to "${this.address}" in 30 minutes!\n\n` +
+      `Total: ${this.total}€\n` +
+      `Cart: "${this.cart.map((item) => item.title).join(", ")}"\n\n` +
+      `Thank you for choosing us!`);
+
+
+  }
 }
